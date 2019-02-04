b6f3dc64a8fd8725bafd84d4e9ca0b6f35833da2
LexineAce <lexine126@knights.ucf.edu>
2019-02-03 23:50:43 -0500

    Challenge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,52 +1,25 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
+﻿using UnityEngine; using UnityEngine.UI; using System.Collections;  public class PlayerController : MonoBehaviour
+{      public float speed;     public Text countText;     public Text winText;     public Text scoreText;     public Text lifeText;     public Text dieText;      private Rigidbody rb;     private int count;     private int score;     private int life;      void Start()     {         rb = GetComponent<Rigidbody>();         score = 0;         count = 0;         life = 3;         SetCountText();         winText.text = "";         dieText.text = "";     }      void FixedUpdate()     {         float moveHorizontal = Input.GetAxis("Horizontal");         float moveVertical = Input.GetAxis("Vertical");          Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);          rb.AddForce(movement * speed);          if (Input.GetKey("escape"))             Application.Quit();     }
 
-public class PlayerController : MonoBehaviour {
 
-    public float speed;
-    public Text countText;
-    public Text winText;
-
-    private Rigidbody rb;
-    private int count;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText();
-        winText.text = "";
-    }
-
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement * speed);
-
-        if (Input.GetKey("escape"))
-            Application.Quit();
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Pick Up"))
+    //changed code
+    void OnTriggerEnter(Collider other)     {         if (other.gameObject.CompareTag("Pick Up"))         {             other.gameObject.SetActive(false);             count = count + 1;             score = score + 1;             SetCountText();         }
+        else if (other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
-            SetCountText();
-        }
-    }
-    void SetCountText() 
-    {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 10)
+            score = score - 1; // this removes 1 from the score             life = life - 1;           SetCountText();
+        }         if (count == 10)
         {
-            winText.text = "You Win!";
-        }
+            transform.position = new Vector3(35.0f, transform.position.y, transform.position.z);
+        }  
     }
-}
+
+    //end change
+
+
+    private void SetCountText()
+    {         countText.text = "Count: " + count.ToString();         scoreText.text = "Score: " + score.ToString();         lifeText.text = "Life: " + life.ToString();         if (score >= 18)         {             winText.text = "You Win!";         }         else if (life <= 0)
+        {
+            dieText.text = "You Died!";
+        }     } } 
---
+++ Assets/Scripts/Slider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour {
+
+    public float speed = 1.0f;
+    Vector3 pointA;
+    Vector3 pointB;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointA = new Vector3(30, 1, -4);
+        pointB = new Vector3(39, 1, -4);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+        
+    }
+}
